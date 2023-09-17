3ef00406f748f7ed3076f4395f8fe7cb163c9c93
Paul R <rudnikov.paul@yandex.ru>
2023-09-18 02:53:25 +0500

    Added two new commands

--- simpleMath.py
+++ simpleMath.py
@@ -2,28 +2,68 @@ import sublime
 import sublime_plugin
 import re
 import time
+import simpleMath.pyperclip as clipboard
+import math
+
 
 class SimpleMathCommand(sublime_plugin.TextCommand):
-    def countSumm(self):
-        # get selected text
+    def display(self, title, value):
+        # copy to clipboard and display operation result
+        clipboard.copy(value)
+        # if value.is_float():
+        if isinstance(value, float):
+            if value.is_integer():
+                value = int(value)
+        sublime.status_message('   ' + title + ': ' + str(value) + "   copied to clipboard")
+
+    def getText(self):
+        # get highlighted text
         sel = self.view.sel()[0]
         if sel.begin() == sel.end():
-            text = self.view.substr(sublime.Region(0, self.view.size()))
+            return self.view.substr(sublime.Region(0, self.view.size()))
         else:
-            text = self.view.substr(sel)
+            return self.view.substr(sel)
 
-        # find summ of all numbers in text
-        numbers = re.findall(r'(\d+\.\d+|\d+)', text)
-        summ = sum([float(i) for i in numbers])
-        if summ.is_integer():
-            summ = int(summ)
-
-        # display result
-        now = time.strftime("%H:%M:%S")
-        print(now + "  " + str(summ))
-        sublime.status_message('   Summ: ' + str(summ) + "   see [ctrl ~] for copying")
+    def getNumbers(self):
+        text = self.getText()
+        result = re.findall(r'(\d+\.\d+|\d+)', text)
+        return [float(x) for x in result]
 
 class SummCommand(SimpleMathCommand):
 
     def run(self, edit):
-        self.countSumm()
+        # find summ of all numbers in text
+        numbers = self.getNumbers()
+        summ = sum(numbers)
+        self.display("Summ", summ)
+
+class MultiplyCommand(SimpleMathCommand):
+
+    def run(self, edit):
+        # multiply all numbers in text
+        numbers = self.getNumbers()
+        multiplied = 1.0
+        for i in numbers:
+            multiplied*=i
+        self.display("Multiplied", multiplied)
+
+class CalculateCommand(SimpleMathCommand):
+
+    def run(self, edit):
+        # calculate math expression
+        expression = self.getText()
+
+        # remove non mathematical symbols
+        expression = "".join(re.findall(r'(\d+\.\d+|\d+|\*|\/|\+|\-|\(|\))', expression))
+
+        # remove whitespaces
+        expression = re.sub(r' ', r'', expression)
+
+        # add multiply characters if needed
+        expression = re.sub(r'(\d)(\()', r'\1*\2', expression)
+        expression = re.sub(r'(\))(\d)', r'\1*\2', expression)
+        try:
+            result = eval(expression)
+            self.display("Result", result)
+        except Exception as e:
+            sublime.status_message("   ERROR: " + repr(e))
